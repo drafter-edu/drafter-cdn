f7bb1b7dff007b647a2c84b57d5a81736d538977
Austin Cory Bart <acbart@vt.edu>
2024-04-04 11:15:15 -0400

    Get latest utility

---
+++ get_latest.bat
@@ -0,0 +1,4 @@
+cp ../drafter/dist-js/skulpt-drafter.js skulpt/
+cp ../../blockpy-edu/skulpt/dist/skulpt.js skulpt/
+cp ../../blockpy-edu/skulpt/dist/skulpt-stdlib.js skulpt/
+cp ../../blockpy-edu/skulpt/dist/skulpt.js.map skulpt/
