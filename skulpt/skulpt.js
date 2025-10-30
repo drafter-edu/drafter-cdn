508a18712d5cc872cb864f307566e52de7c428dc
Austin Cory Bart <acbart@vt.edu>
2025-10-30 12:14:39 -0400

    Don't lose hash

--- skulpt/skulpt.js
+++ skulpt/skulpt.js
@@ -26,7 +26,7 @@
       var Sk2 = {};
       Sk2.build = {
         githash: "2c0cb427",
-        date: "2025-10-30T16:08:26.658Z"
+        date: "2025-10-30T16:14:24.120Z"
       };
       Sk2.global = typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {};
       Sk2.exportSymbol = function(name, object) {
